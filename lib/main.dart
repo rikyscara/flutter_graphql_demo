6f20a4e090a458943dd046d9b22f7be09cf61759
riccardo scaramagli <riccardo.scaramagli@uberall.com>
2024-07-31 17:58:17 +0200

    remove supabase reference

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'application/cubit/film_cubit.dart';
 import 'data/film_repository.dart';
@@ -9,21 +8,11 @@ import 'presentation/pages/film_list.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  const supabaseUrl = 'https://zrrgzwbdxkgbksjkptqd.supabase.co';
-  const supabaseKey = String.fromEnvironment('SUPABASE_KEY');
-
-  final supabase = await Supabase.initialize(
-    url: supabaseUrl,
-    anonKey: supabaseKey,
-  );
-
-  runApp(MainApp(db: supabase));
+  runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key, required this.db});
-
-  final Supabase db;
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
